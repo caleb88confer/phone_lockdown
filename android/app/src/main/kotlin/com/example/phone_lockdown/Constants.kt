0320b148d13e5e6f0eeca49be8e712b7031fd4f0
Caleb Confer <caleb88confer@gmail.com>
2026-04-07 17:09:17 -0400

    feat: add centralized Android Constants object

---
+++ android/app/src/main/kotlin/com/example/phone_lockdown/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.phone_lockdown
+
+object Constants {
+    // SharedPreferences keys
+    const val PREF_IS_BLOCKING = "isBlocking"
+    const val PREF_BLOCKED_PACKAGES = "blockedPackages"
+    const val PREF_BLOCKED_WEBSITES = "blockedWebsites"
+    const val PREF_ACTIVE_PROFILE_BLOCKS = "activeProfileBlocks"
+    const val PREF_FAILSAFE_ALARMS = "failsafeAlarms"
+
+    // Method channel
+    const val METHOD_CHANNEL = "app.phonelockdown/blocker"
+}
