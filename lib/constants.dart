6434bee1df28973ec0c33a55626c47d895c70c4a
Caleb Confer <caleb88confer@gmail.com>
2026-04-07 17:03:53 -0400

    feat: add centralized Flutter constants file

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+// Method channel
+const kMethodChannel = 'app.phonelockdown/blocker';
+
+// SharedPreferences keys
+const kPrefSavedProfiles = 'savedProfiles';
+const kPrefCurrentProfileId = 'currentProfileId';
+const kPrefActiveLocks = 'activeLocks';
+const kPrefIsBlocking = 'isBlocking';
+
+// Defaults
+const kDefaultFailsafeMinutes = 1440;
